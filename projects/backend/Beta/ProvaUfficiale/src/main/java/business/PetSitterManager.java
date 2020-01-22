6c193f4670cf30b1b0aef4205cbbc02dbd0434e6
alexdicarmine <alex.dicarmine@hotmail.it>
2020-01-22 15:01:09 +0100

    2020/01/22

---
+++ projects/backend/Beta/ProvaUfficiale/src/main/java/business/PetSitterManager.java
@@ -0,0 +1,17 @@
+package business;
+
+import java.util.List;
+
+import interfaces.PetSitterInterface;
+import model.Evento;
+import model.UtenteApp;
+
+public class PetSitterManager implements PetSitterInterface {
+
+	public List<Evento> visualizzaEventi(UtenteApp utente) {
+		
+		
+		return null;
+	}
+
+}
--- projects/backend/Beta/ProvaUfficiale/src/main/java/interfaces/UtenteAppInterface.java
+++ projects/backend/Beta/ProvaUfficiale/src/main/java/interfaces/UtenteAppInterface.java
@@ -1,5 +1,12 @@
 package interfaces;
 
+import java.util.List;
+
+import model.Evento;
+import model.UtenteApp;
+
 public interface UtenteAppInterface {
+	
+	abstract List<Evento> visualizzaEventi(UtenteApp utente);
 
 }
