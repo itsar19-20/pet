c480ad49c0d86a8ed5a7e664f9b440bb33a500f1
gabriele <galigetta@hptmail.it>
2020-01-31 13:32:08 +0100

    Index con immagini

--- projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/index.js
+++ projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/index.js
@@ -6,5 +6,24 @@ $(() => {
         if((utente_session.tipoUtente).contentEquals("proprietario")){
             $('#home').hide();
         }
-	}
+    }
+    
 })
+
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
