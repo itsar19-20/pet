eb6a1b6d00ae7f770b80c78d623219f1644873f4
Gionesci00 <g.nesci@itsrizzoli.it>
2020-01-31 17:10:14 +0100

    ...

--- projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/index.js
+++ projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/index.js
@@ -1,16 +1,4 @@
 $(() => {
-<<<<<<< HEAD
-	if(localStorage.getItem('user') || sessionStorage.getItem('user')){
-        var utente_local = localStorage.getItem('user');
-        var utente_session = sessionStorage.getItem('user');
-       // ${utente_local.tipoUtente}
-        if((utente_session.tipoUtente).contentEquals("proprietario")){
-            $('#home').hide();
-        }
-    }
-    
-=======
-
         $('#logout').hide();
         $('#admin').hide();
 
@@ -39,5 +27,4 @@ $(() => {
                 location.href('./index.html')
             })
 	
->>>>>>> b7f8f718ae165ffc98ef3ce8c4cad93c56b9835c
 })
