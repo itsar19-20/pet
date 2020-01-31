c629eba1112ff044eb2c1a710ea9350796e8979c
Gionesci00 <g.nesci@itsrizzoli.it>
2020-01-31 16:13:35 +0100

    admin html e js

--- projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/admin.js
+++ projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/admin.js
@@ -1,5 +1,18 @@
 $(() => {
 	$('#homepage_admin').click(() => {
 		location.href = './index.html';
+		$('#homepage_admin').active();
 	})
+	$('#gestioneUtenti_admin').click(() => {
+		$('#gestioneUtenti_admin').active();
+	})
+	$('#segnalazioni').click(() => {
+		location.href = './index.html';
+		$('#segnalazioni_admin').active();
+	})
+	$('#statistiche_admin').click(() => {
+		location.href = './index.html';
+		$('#statistiche_admin').active();
+	})
+	
 })
