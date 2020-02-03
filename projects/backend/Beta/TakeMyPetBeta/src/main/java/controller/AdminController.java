86806684de252afe0493faa03381b81ead9ea6e5
Gionesci00 <g.nesci@itsrizzoli.it>
2020-02-03 22:07:54 +0100

    data table (da aggiustare) 2020/03/02

---
+++ projects/backend/Beta/TakeMyPetBeta/src/main/java/controller/AdminController.java
@@ -0,0 +1,58 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import business.AdminManager;
+import model.UtenteApp;
+
+/**
+ * Servlet implementation class AdminController
+ */
+@WebServlet("/AdminController")
+public class AdminController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdminController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+
+			AdminManager am = new AdminManager();
+			List<UtenteApp> listaUtenti = new ArrayList<UtenteApp>();
+			ObjectMapper om = new ObjectMapper();
+			listaUtenti = am.visualizzaUtenti();
+			
+			response.setContentType("application/json");
+			response.getWriter().append(om.writeValueAsString(listaUtenti));
+			
+			
+		}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
