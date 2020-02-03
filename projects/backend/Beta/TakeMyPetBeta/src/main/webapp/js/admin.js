68ccd9d05794a88e7a37d7e852c08eac01ea2421
Gionesci00 <g.nesci@itsrizzoli.it>
2020-02-03 10:40:44 +0100

    handsontable 2020/03/02

--- projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/admin.js
+++ projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/admin.js
@@ -1,15 +1,19 @@
-/*$(() => {
+$(() => {
 	$('#homepage_admin').click(() => {
 		location.href = './index.html';
 		document.getElementById('homepage_admin').style.color = '#eeeeee';
 	})
 	$('#gestioneUtenti_admin').click(() => {
 		document.getElementById('gestioneUtenti_admin').style.color = '#14a302';
+		document.getElementById('statistiche_admin').style.color = '#14a302';
+		document.getElementById('gestioneUtenti_admin').style.color = '#14a302';
+		document.getElementById('gestioneUtenti_admin').style.color = '#14a302';
 	})
+	/*
 	$('#segnalazioni_admin').click(() => {
 		document.getElementById('segnalazioni_admin').style.background = '#eeeeee';
 	})
 	$('#statistiche_admin').click(() => {
 		document.getElementById('statistiche_admin').style.color = '#14a302';	})
-	
-})*/
+	*/
+})
