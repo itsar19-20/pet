e18e462543f2884adfb7a6f0de0c36c80b6c64dd
Gionesci00 <g.nesci@itsrizzoli.it>
2020-02-03 11:34:04 +0100

    admin js 20202/03/02

--- projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/admin.js
+++ projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/admin.js
@@ -3,17 +3,22 @@ $(() => {
 		location.href = './index.html';
 		document.getElementById('homepage_admin').style.color = '#eeeeee';
 	})
-	$('#gestioneUtenti_admin').click(() => {
-		document.getElementById('gestioneUtenti_admin').style.color = '#14a302';
-		document.getElementById('statistiche_admin').style.color = '#14a302';
-		document.getElementById('gestioneUtenti_admin').style.color = '#14a302';
-		document.getElementById('gestioneUtenti_admin').style.color = '#14a302';
-	})
-	/*
-	$('#segnalazioni_admin').click(() => {
-		document.getElementById('segnalazioni_admin').style.background = '#eeeeee';
-	})
 	$('#statistiche_admin').click(() => {
-		document.getElementById('statistiche_admin').style.color = '#14a302';	})
-	*/
+		document.getElementById('homepage_admin').style.color = '#eeeeee';
+		document.getElementById('statistiche_admin').style.color = '#14a302';
+		document.getElementById('gestioneUtenti_admin').style.color = '#eeeeee';
+		document.getElementById('segnalazioni_admin').style.color = '#eeeeee';
+	})
+	$('#gestioneUtenti_admin').click(() => {
+		document.getElementById('homepage_admin').style.color = '#eeeeee';
+		document.getElementById('statistiche_admin').style.color = '#eeeeee';
+		document.getElementById('gestioneUtenti_admin').style.color = '#14a302';
+		document.getElementById('segnalazioni_admin').style.color = '#eeeeee';
+	})
+	$('#segnalazioni_admin').click(() => {
+		document.getElementById('homepage_admin').style.color = '#eeeeee';
+		document.getElementById('statistiche_admin').style.color = '#eeeeee';
+		document.getElementById('gestioneUtenti_admin').style.color = '#eeeeee';
+		document.getElementById('segnalazioni_admin').style.color = '#14a302';
+	})
 })
