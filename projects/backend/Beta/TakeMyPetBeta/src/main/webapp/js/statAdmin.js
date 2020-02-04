7bc15ba659b95d631b264681bcc8dcfe1933784b
alexdicarmine <alex.dicarmine@hotmail.it>
2020-02-04 17:41:10 +0100

    2/4/2020 Grafico FUNZIONANTE(Momemnto più bello fino ad ora)

--- projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/statAdmin.js
+++ projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/statAdmin.js
@@ -1,37 +1,44 @@
 $(() =>{
+    var dataPoints=[];
+
+var options =  {
+	animationEnabled: true,
+	theme: "light2",
+	title: {
+		text: "Daily New Users"
+	},
+	axisX: {
+		valueFormatString: "DD MMM YYYY",
+	},
+	axisY: {
+		title: "Users",
+		titleFontSize: 24,
+		includeZero: false
+	},
+	data: [{
+		type: "spline",
+		yValueFormatString: "$#,###.##",
+		dataPoints: dataPoints
+	}]
+};
+
 	$.ajax({
 		url: '/statistica',
 		method: 'get'
 	})
 	.done((stat) => {
 		if(stat){
-            var ctx = document.getElementById("myChart");
-            var myChart = new Chart(ctx, {
-              type: 'line',
-              data: {
-                labels: data.dataRegistrazione,
-                datasets: [{
-                  data: data.dataReg,
-                  lineTension: 0,
-                  backgroundColor: 'transparent',
-                  borderColor: '#14a302',
-                  borderWidth: 4,
-                  pointBackgroundColor: '#14a302'
-                }]
-              },
-              options: {
-                scales: {
-                  yAxes: [{
-                    ticks: {
-                      beginAtZero: false
-                    }
-                  }]
-                },
-                legend: {
-                  display: false,
-                }
-              }
-            });
-		}
+
+            for (var i = 0; i < stat.length; i++) {
+
+
+                dataPoints.push({
+                    x: new Date(stat[i][0]),
+                    y: stat[i][1]
+                });
+            }
+
+        }
+        $("#chartContainer").CanvasJSChart(options);
 	})
 })
