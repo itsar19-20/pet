5d7bda1cbbf049b45b4515c096d6db882f9db0ba
alexdicarmine <57755176+alexdicarmine@users.noreply.github.com>
2020-02-07 17:43:59 +0100

    Update admin.js

--- projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/admin.js
+++ projects/backend/Beta/TakeMyPetBeta/src/main/webapp/js/admin.js
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-$(() => {
-		$('#homepage_admin').click(() => {
-		document.getElementById('homepage_admin').style.color = '#14a302';
-		document.getElementById('statistiche_admin').style.color = '#333';
-		document.getElementById('gestioneUtenti_admin').style.color = '#333';
-		document.getElementById('segnalazioni_admin').style.color = '#333';
-		location.href = './index.html';
-	})
-	document.getElementById('statistiche_admin').style.color = '#14a302';
-	$('#statistiche_admin').click(() => {
-		document.getElementById('homepage_admin').style.color = '#333';
-		document.getElementById('statistiche_admin').style.color = '#14a302';
-		document.getElementById('gestioneUtenti_admin').style.color = '#333';
-		document.getElementById('segnalazioni_admin').style.color = '#333';
-	})
-	$('#gestioneUtenti_admin').click(() => {
-		document.getElementById('homepage_admin').style.color = '#333';
-		document.getElementById('statistiche_admin').style.color = '#333';
-		document.getElementById('gestioneUtenti_admin').style.color = '#14a302';
-		document.getElementById('segnalazioni_admin').style.color = '#333';
-	})
-	$('#segnalazioni_admin').click(() => {
-		document.getElementById('homepage_admin').style.color = '#333';
-		document.getElementById('statistiche_admin').style.color = '#333';
-		document.getElementById('gestioneUtenti_admin').style.color = '#333';
-		document.getElementById('segnalazioni_admin').style.color = '#14a302';
-	})
-})
-=======
+
 $(() => {
 		$('#homepage_admin').click(() => {
 		document.getElementById('homepage_admin').style.color = '#14a302';
@@ -75,4 +46,4 @@ $(() => {
 	})
 })
 
->>>>>>> refs/remotes/origin/Newgiorgio
+
